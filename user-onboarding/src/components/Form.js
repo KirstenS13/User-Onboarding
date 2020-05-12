c9bca18aaf06a1f1cdb9f5394bcd97426ba0235c
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-12 19:13:09 -0400

    created name input area

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,9 +1,33 @@
+//import React
 import React from 'react';
 
+/* the Form needs:
+    Name
+    Email
+    Password
+    Checkbox (Terms and Conditions)
+    Submit Button
+*/
+
+//create Form function to render the form
 function Form(){
+    //State, functions, hooks in here (outside the return)
+
+    //JSX inside the return
     return (
-        <p>This will soon be a form.</p>
+        <form>
+            <label htmlFor='name'>
+                Name:
+                <input 
+                    type='text' 
+                    name='name' 
+                    id='name' 
+                    placeholder='Enter your full name...'
+                />
+            </label>
+        </form>
     );
 }
 
+//default export the form
 export default Form;
